45e35f8a2dd5096f19bcfb1a35d20a5b891db04c
David Molinero <davidmolinero.com@gmail.com>
2019-06-01 18:07:52 +0100

    Generate new valid contract

--- spec/payment_service_client_spec.rb
+++ spec/payment_service_client_spec.rb
@@ -21,6 +21,25 @@ describe PaymentServiceClient, :pact => true do
     end
   end
 
+  context "given another valid payment method" do
+    let(:valid_payment_method) { "1111222233334444" }
+    let(:response_body) do { status: :valid } end
+    before do
+      payment_service
+        .upon_receiving("a request for validating another payment method")
+        .with(method: :get, path: "/validate-payment-method/#{valid_payment_method}")
+        .will_respond_with(
+          status: 200,
+          headers: {"Content-Type" => "application/json"},
+          body: response_body
+        )
+    end
+
+    it "the call to payment service returns a payment status response with status equal to valid" do
+      expect(subject.validate(valid_payment_method)).to eql({ "status" => "valid" })
+    end
+  end
+
   context "given a black listed payment method" do
     let(:invalid_payment_method) { "9999999999999999" }
     let(:response_body) do { status: :fraud } end
